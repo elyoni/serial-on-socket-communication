cfdc4f166b1668bde251c3ba9523984d5968de79
Yoni Elentok <yehonatan.e@solaredge.com>
2018-08-31 22:38:18 +0300

    Minor improvments

--- server_serial.py
+++ server_serial.py
@@ -3,20 +3,26 @@ import socket
 import _thread
 import traceback
 import select
+import os
 import sys # used to check what platform i am using
 import platform
 import serial
 
 import time
 
-
 DEBUG = True
+serial_connection = '/dev/ttyUSB1'
+
+def print_debug(string_to_print):
+    if DEBUG:
+        print(string_to_print)
 
 class ServerSocket(object):
+    """ The class is connection a socket connetion to serial connection """
     def __init__(self,host='127.0.0.1', port=50000, max_connection=5):
-        self.ser = serial.Serial('/dev/ttyUSB1', 115200, timeout=1)
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# Create the Server Socket
         try:
+            self.ser = serial.Serial(serial_connection, 115200, timeout=1)
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# Create the Server Socket
             print('Socket Server is on')
             print('<<<<Press any key to exit>>>>')
             server_address = (host, port)
@@ -28,17 +34,29 @@ class ServerSocket(object):
             if platform.system() == 'Linux':
                 self.input.append(sys.stdin) #not working on windows
             elif platform.system() == 'Win':
-                print('The system is windows, input from keyboard is not supported')
+                print_debug('The system is windows, input from keyboard is not supported')
             
             # Sockets to which we expect to write
             self.output = []
+        except FileNotFoundError as e:
+            print("There is no", serial_connection, "The server cannot start running, make sure the device is connected")
+            raise
+        except serial.serialutil.SerialException as e:
+            print(e)
+            raise
         except Exception as e:
             print('Problem with creating the Server', e)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print("[" + os.path.basename(__file__) + "]", "Exception:", e,
+                  exc_type, " error line:", exc_tb.tb_lineno)
+
+            raise
     
     def send(self, connection, data):
         connection.send(data.encode('ascii'))
 
     def receive(self, connection):
+        """ The function collect the sokect msg and send it to the serial device """
         data = connection.recv(1024)
         if data:
             if (data.decode('ascii') == "?id"):
@@ -54,14 +72,14 @@ class ServerSocket(object):
             connection.close()
 
     def execute_server(self):
+        """ Handeling the message/connection that is comming from the socket connection """
         exit = False
         while not exit:
             try:
                 input_ready, output_ready, except_ready = select.select(self.input , self.output, [])
                 for connection in input_ready:  # work on all the input that send something
                     if connection is self.server:
-                        if DEBUG:
-                            print("New connection")
+                        print_debug("New connection")
                         connection, client_address = connection.accept()
                         connection.setblocking(0)
                         self.input.append(connection)
@@ -78,5 +96,8 @@ class ServerSocket(object):
         print("The server is close")
 
 if __name__ == "__main__":
-    server_socket = ServerSocket()
-    server_socket.execute_server()
+    try:
+        server_socket = ServerSocket()
+        server_socket.execute_server()
+    except:
+        print("ERROR, The program will stop")
